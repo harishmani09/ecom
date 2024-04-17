a3b7f6363c0fb5eda877bfd1b7419940b953b7c0
harishmani <harishmani@gmail.com>
2024-04-17 22:19:14 +0530

    store variation done

--- store/urls.py
+++ store/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 urlpatterns = [
     path('store/',views.store, name='store'),
-    path("store/<slug:category_slug>/",views.store,name="products_by_category"),
-    path("store/<slug:category_slug>/<slug:product_slug>/",views.product_detail,name="product_detail"),
-    path('store/<int:product_id>',views.product_detail, name='product_detail'),
+    path("store/category/<slug:category_slug>/",views.store,name="products_by_category"),
+    path("store/category/<slug:category_slug>/<slug:product_slug>/",views.product_detail,name="product_detail"),
+    path('store/<int:product_id>/',views.product_detail, name='product_detail'),
+    path("store/search/", views.search,name='search'),
 
 ]
