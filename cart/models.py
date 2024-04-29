717a9a42a528a0b048e0108622d7e37369fcfe69
harishmani <harishmani@gmail.com>
2024-04-29 19:11:08 +0530

    checkout template

--- cart/models.py
+++ cart/models.py
@@ -1,26 +1,28 @@
 from typing import Any
 from django.db import models
-from store.models import Product,Variation
+from store.models import Product, Variation
+from accounts.models import Account
+
+
 # Create your models here.
 class Cart(models.Model):
     cart_id = models.CharField(max_length=100, blank=True)
     date_added = models.DateField(auto_now_add=True)
-    
+
     def __str__(self) -> None:
         return self.cart_id
-    
+
 
 class CartItem(models.Model):
-    product=models.ForeignKey(Product,on_delete=models.CASCADE)
-    variations=models.ManyToManyField(Variation,blank=True)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
-    
-    
+
     def sub_total(self):
-        return self.product.price * self.quantity 
-    
+        return self.product.price * self.quantity
+
     def __unicode__(self) -> str:
         return self.product
-    
